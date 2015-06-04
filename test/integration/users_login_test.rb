242fdde25311d8ce93bb62927ec697747293446d
Tutak <rasheed.tutakhail@gmail.com>
2015-06-04 14:02:46 +0000

    add remember me checkbox and test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,7 +17,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
   
   test "login with valid information followed by logout" do
-    get  login_path
+     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
     assert is_logged_in?
     assert_redirected_to @user
@@ -29,6 +29,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
