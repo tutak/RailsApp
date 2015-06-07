1a02e02ec8f6834d37d8bd635641a2ae0061b209
Tutak <rasheed.tutakhail@gmail.com>
2015-06-07 18:27:28 +0000

    list all users in index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+  end
+  
+  def index
+    @users = User.all
   end
   
   def create
