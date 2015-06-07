b7360be25f095b205e30f26eddc4e7a58c765977
Tutak <rasheed.tutakhail@gmail.com>
2015-06-07 09:20:11 +0000

    redirect to proper URL

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,4 +43,16 @@ module SessionsHelper
     @current_user = nil
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
