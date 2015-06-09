38c30281fb2d4ddf70971dedb96382ecff3a2a62
Tutak <rasheed.tutakhail@gmail.com>
2015-06-09 19:24:27 +0000

    add delete users functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
   def show
     @user = User.find(params[:id])
   end
@@ -59,6 +61,15 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
+  end
 end
   
  
