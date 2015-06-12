47f7f3f2d8552189da3f7ad6809b2ad482ff1c2c
Tutak <rasheed.tutakhail@gmail.com>
2015-06-12 06:14:48 +0000

    add mailer iteration 2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,10 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.account_activation.subject
-  #
-  def account_activation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
+  
+ def account_activation(user)
+    @user = user
+    mail to: user.email, subject: "Account activation"
+end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -20,10 +15,5 @@ class UserMailer < ApplicationMailer
     @greeting = "Hi"
 
     mail to: "to@example.org"
-  end
-  
-  def account_activation(user)
-    @user = user
-    mail to: user.email, subject: "Account activation"
   end
 end
